f4d7db90d2f4aaf8b0779fac0ed8e839ee9a477d
Aleksandr Sorokin <me@snay.me>
2024-10-05 13:22:16 +0400

    Implement flashcard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlashcardComponent } from './flashcard/flashcard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FlashcardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/flashcard/flashcard.component.ts
+++ src/app/flashcard/flashcard.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-flashcard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './flashcard.component.html',
   styleUrl: './flashcard.component.css'
 })
 export class FlashcardComponent {
+  words: any[] = [];
+  currentWord: any;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7073/words').subscribe(data => {
+      this.words = data as any[];
+      this.nextWord();
+    });
+  }
+
+  nextWord(): void {
+    const randomIndex = Math.floor(Math.random() * this.words.length);
+    this.currentWord = this.words[randomIndex];
+  }
+
+  checkAnswer(selectedTranslation: string): void {
+    if (selectedTranslation === this.currentWord.translation) {
+      alert('Correct!');
+    } else {
+      alert('Incorrect!');
+    }
+    this.nextWord();
+  }
 }
