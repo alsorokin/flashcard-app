4fd7a34b52518987083a04e477a5214dd6569853
Aleksandr Sorokin <me@snay.me>
2024-10-27 19:59:39 +0400

    Display word collection list in settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
+import { getAllTags } from '../words';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
   isCollapsed: boolean = true;
+  wordCollections: string[];
 
   constructor() {
+    this.wordCollections = getAllTags();
   }
 
   toggleCollapse(): void {
