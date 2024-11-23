5039d62e4abbd04785d25d2e42e0c7e4142086ac
Aleksandr Sorokin <me@snay.me>
2024-11-23 20:40:53 +0400

    Added Angular Material, collection selection dropdown

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(MatSelectModule),
+    importProvidersFrom(MatFormFieldModule),
+    importProvidersFrom(FormsModule),
+  ]
 };
--- src/app/collection-editor/collection-editor.component.ts
+++ src/app/collection-editor/collection-editor.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
-import { WordsService } from '../words.service';
+import { WordCollection, WordsService } from '../words.service';
 import { Word, getAllWords } from '../words';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-collection-editor',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ CommonModule, FormsModule, MatFormFieldModule, MatSelectModule ],
   templateUrl: './collection-editor.component.html',
   styleUrl: './collection-editor.component.css'
 })
 export class CollectionEditorComponent {
-  allWords: Word[];
+  allWords: Word[] = [];
+  allCollections: WordCollection[] = [];
 
-  constructor(private wordsService: WordsService) {
+  constructor(private wordsService: WordsService) { }
+
+  ngOnInit() {
     this.allWords = getAllWords();
+    this.allCollections = this.wordsService.getWordCollections();
+  }
+
+  onSubmit() {
+    console.log(this.allWords);
   }
 }
