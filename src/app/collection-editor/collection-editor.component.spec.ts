aab1d892dbacda0b04427af86485d8dcf1872028
Aleksandr Sorokin <me@snay.me>
2024-11-23 20:55:01 +0400

    Fix tests using Copilot (I have literally no idea what is going on there)

--- src/app/collection-editor/collection-editor.component.spec.ts
+++ src/app/collection-editor/collection-editor.component.spec.ts
@@ -1,16 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CollectionEditorComponent } from './collection-editor.component';
 
+let component: CollectionEditorComponent;
+let fixture: ComponentFixture<CollectionEditorComponent>;
+
 describe('CollectionEditorComponent', () => {
-  let component: CollectionEditorComponent;
-  let fixture: ComponentFixture<CollectionEditorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CollectionEditorComponent]
-    })
-    .compileComponents();
+      imports: [CommonModule, BrowserAnimationsModule, CollectionEditorComponent],
+      declarations: []
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CollectionEditorComponent);
     component = fixture.componentInstance;
